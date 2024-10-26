df53c223b0f1a49cfa2e6311799ad186b37d4ba3
Luis Eduardo Dizu Mendez <dizu.eduardo@gmail.com>
2024-10-26 17:05:26 -0500

    Secciones de hobbies y skills

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function toggleTheme() {
 
 // Escuchar el evento de clic en el botón
 toggleButton.addEventListener('click', toggleTheme);
+
+
+
+document.querySelectorAll('.skill__item').forEach(item => {
+    item.addEventListener('mouseover', () => {
+        const description = item.querySelector('.skill-description');
+        description.style.display = 'block'; // Muestra la descripción al pasar el mouse
+    });
+
+    item.addEventListener('mouseout', () => {
+        const description = item.querySelector('.skill-description');
+        description.style.display = 'none'; // Oculta la descripción al salir el mouse
+    });
+});
